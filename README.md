4b37bf991e2a28b622c04a968966ed5e7587334a
unknown <dockerjan2018@gmail.com>
2019-11-02 20:44:50 +0000

    this project has just one file

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # my-first-repo
 this is to demonstrate the kickstart of github and git
+
+#this file has some random python based commands
+`python firstprog.py`
---
+++ firstprog.py
@@ -0,0 +1,7 @@
+#ye hai ek sample code... jaha print command ka upyog ho ra hai
+print("hi... I am alive")
+#yeh hai ek  loop
+for x in range(11):
+    print(2*x)
+for x in range(11):
+    print('*'*x)
